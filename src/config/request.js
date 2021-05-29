0583ece799f6cc32aa35f63345421a0fae13a677
huangst <313949553@qq.com>
2021-05-29 15:29:21 +0800

    通信test

--- src/config/request.js
+++ src/config/request.js
@@ -5,6 +5,44 @@ uniRequest.defaults.baseURL = process.env.VUE_APP_BASE_API;
 // uniRequest.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 uniRequest.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
+// 获取app请求头数据
+// var isAndroid = navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('Adr') > -1; //android终端
+// var isiOS = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+// // 这是必须要写的，用来初始化一些设置
+// function setupWebViewJavascriptBridge(callback) {
+// 	if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge); }
+// 	if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
+// 	window.WVJBCallbacks = [callback];
+// 	var WVJBIframe = document.createElement('iframe');
+// 	WVJBIframe.style.display = 'none';
+// 	WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+// 	document.documentElement.appendChild(WVJBIframe);
+// 	setTimeout(function() { document.documentElement.removeChild(WVJBIframe) }, 0)
+// }	
+// // ios
+// if (isiOS) {
+// 	console.log('这是IOS');
+// 	let params = {};
+// 	//这也是固定的， OC 调JS ， 需要给OC调用的函数必须写在这个函数里面
+// 	setupWebViewJavascriptBridge(function(bridge) {
+// 		bridge.registerHandler('testJSFunction', function(data, responseCallback) {
+// 			alert('JS方法被调用:'+data);
+// 			responseCallback('js执行过了');
+// 		})
+// 	})
+// 	WebViewJavascriptBridge.callHandler('getLoginUserInfo',params,function(response) {
+// 		console.log(response);
+// 	});
+// }
+// // Android
+// if (isAndroid) {
+// 	console.log('这是Android');
+// 	let params = {};
+// 	if(Android){
+// 		Android.getLoginUserInfo(params);
+// 	}	
+// }   
+
 // 请求拦截
 uniRequest.interceptors.request.use(
 	config => {
