1a3a6cd9398caa43f0f5ec773ca526af8e32c3e5
fanrk <442189385@qq.com>
2021-06-07 20:06:13 +0800

    协议

--- src/pages/protocol/tyService.vue
+++ src/pages/protocol/tyService.vue
@@ -4,142 +4,105 @@
 			<text slot="title">电子合同</text>
 		</WhiteHeader>
 		
-		<view class="contract-page__header">福建大道成物流科技有限公司无车承运平台运输电子合同</view>
+		<view class="contract-page__header">天翼账号服务协议</view>
 		
-		<view class="contract-page__header-right">
-			<view class="text">合同编号：<text class="underline">{{ form.contractNo }}</text></view>
-			<view class="text">签约时间：<text class="underline">{{ form.createTime }}</text></view>
-		</view>
-	
-		<view class="contract-page__header-left">
-			<view>甲方：<text class="underline">{{ form.branchName }}</text></view>
-			<view>统一社会信用代码：<text class="underline">{{ form.branchOrganizationCodeNo }}</text></view>
-			<view>联系电话：<text class="underline">{{ form.branchPhone }}</text></view>
-		</view>
+		<view class="contract-page__content">欢迎您使用天翼账号平台（下面简称“平台”），为确保用户的合法权益及正常商业秩序（“用户”是指使用平台相关服务的使用人，在本协议中更多地称为“您”。），特制订天翼账号平台服务协议（下面简称“本协议”），本协议由您与平台的经营者共同缔结，本协议具有合同效力。</view>
 		
-		<view class="contract-page__header-left">
-			<view>乙方：<text class="underline">{{ form.driverName }}</text></view>
-			<view>车牌号码：<text class="underline">{{ form.licenseNumber }}</text></view>
-			<view>驾驶证号码(选填)：<text class="underline">{{ form.driverLicense }}</text></view>
-			<view>身份证号码：<text class="underline">{{ form.driverIdentificationNumber }}</text></view>
-			<view>联系电话：<text class="underline">{{ form.driverPhone }}</text></view>
-		</view>
+		<view class="contract-page__title">一、协议内容及签署</view>
+		<view class="contract-page__mintitle">1.1.本协议内容包括协议正文及所有平台已经发布的或将来可能发布的各类规则。所有规则为本协议不可分割的组成部分，与协议正文具有同等法律效力。除另行明确声明外，任何平台及其关联公司提供的服务（以下称为平台服务）均受本协议约束。</view>
+		<view class="contract-page__mintitle">1.2.您应当在使用平台服务之前认真阅读全部协议内容，对于协议中以加粗字体显示的内容，您应重点阅读。如您对协议有任何疑问的，应向平台咨询。但无论您事实上是否在使用平台服务之前认真阅读了本协议内容，只要您使用平台服务，则本协议即对您产生约束，届时您不应以未阅读本协议的内容或者未获得平台对您问询的解答等理由，主张本协议无效，或要求撤销本协议。</view>
+		<view class="contract-page__mintitle">1.3.您承诺接受并遵守本协议的约定。如果您不同意本协议的约定，您应立即停止注册激活程序或停止使用平台服务。</view>
+		<view class="contract-page__mintitle">1.4.平台有权根据需要不时地制订、修改本协议及或各类规则，并以网站公示的方式进行公告，不再单独通知您。变更后的协议和规则一经在网站公布后，立即自动生效。如您不同意相关变更，应当立即停止使用平台服务。您继续使用平台服务的，即表示您接受经修订的协议和规则。</view>
 		
-		<view class="contract-page__content">本合同适用于福建大道成物流科技有限公司超好运网络货运平台（以下简称“超好运平台”）所有注册认证的司机会员。甲乙双方达成交易，由甲方委托乙方完成货物运输。双方就货物运输事宜达成以下协议以资共守。</view>
+		<view class="contract-page__title">二、协议范围</view>
+		<view class="contract-page__mintitle">2.1.本协议是您与平台之间关于用户使用平台相关服务所订立的协议。“平台”是指平台及其相关服务可能存在的运营关联单位。</view>
+		<view class="contract-page__mintitle">2.2.本协议项下的服务是指平台向用户提供的包括但不限于通讯功能、支付功能、存储功能和广告等产品及服务（以下简称“本服务”）。</view>
+		<view class="contract-page__mintitle">2.3.本协议内容同时包括补充协议，且您在使用平台某一特定服务时，该服务可能会另有单独的协议、相关业务规则等（以下统称为“单独协议”）。上述内容一经正式发布，即为本协议不可分割的组成部分，您同样应当遵守。您对前述任何业务规则、单独协议的接受，即视为您对本协议全部的接受。</view>
+		<view class="contract-page__mintitle">2.4 关于中国移动及中国联通用户协议可查阅《中国移动认证服务协议》 、《中国联通服务条款及隐私协议》。</view>
 		
-		<view class="contract-page__title">一、 本批货物内容、运输要求及支付方式见超好运平台上由甲方发布并由乙方确认接受的运单。货物信息表如下：</view>
-		<view class="contract-page__table">
-			<uni-table border emptyText="暂无数据">
-			    <tbody>
-					<uni-tr>
-					   <uni-td><b>起运地</b></uni-td>
-					   <uni-td>{{ form.startAddress }}</uni-td>
-					   <uni-td><b>发货人</b></uni-td>
-					   <uni-td>{{ form.consignor }}</uni-td>
-					   <uni-td><b>手机</b></uni-td>
-					   <uni-td>{{ form.consignorPhone }}</uni-td>
-					</uni-tr>
-					<uni-tr>
-						<uni-td><b>目的地</b></uni-td>
-						<uni-td>{{ form.endAddress }}</uni-td>
-						<uni-td><b>收货人</b></uni-td>
-						<uni-td>{{ form.consignee }}</uni-td>
-						<uni-td><b>手机</b></uni-td>
-						<uni-td>{{ form.consigneePhone }}</uni-td>
-					</uni-tr>
-					<uni-tr>
-						<uni-td><b>实际承运方</b></uni-td>
-						<uni-td>{{ form.driverName }}</uni-td>
-						<uni-td><b>联系电话</b></uni-td>
-						<uni-td>{{ form.driverPhone }}</uni-td>
-						<uni-td><b>货物名称</b></uni-td>
-						<uni-td>{{ form.goodsTypeName || form.goodsBigTypeName }}</uni-td>
-					</uni-tr>
-					<uni-tr>
-						<uni-td><b>最高配载</b></uni-td>
-						<uni-td>
-							{{ form.loadWeight }}
-							{{ form.stowageStatus == '0' ? '吨' : (form.stowageStatus == '1'? '立方': '车') }}
-						</uni-td>
-						<uni-td><b>货物描述</b></uni-td>
-						<uni-td>{{ form.goodsName }}</uni-td>
-						<uni-td><b>货主应付金额（元）</b></uni-td>
-						<uni-td>￥{{ form.deliveryFeeDeserved }}</uni-td>
-					</uni-tr>
-					<uni-tr>
-						<uni-td><b>合同签订时间</b></uni-td>
-						<uni-td>{{ form.createTime }}</uni-td>
-						<uni-td><b>货物装车时间</b></uni-td>
-						<uni-td>{{ form.loadTime }}</uni-td>
-						<uni-td><b>是否开票</b></uni-td>
-						<uni-td>{{ '是' }}</uni-td>
-					</uni-tr>
-					<uni-tr>
-						<uni-td><b>备注</b></uni-td>
-						<uni-td colspan="5">{{ form.orderRemark }}</uni-td>
-					</uni-tr>
-			    </tbody>
-			</uni-table>
-		</view>
+		<view class="contract-page__title">三、注册与账号</view>
 		
-		<view class="contract-page__title">二、在本次运单所含运费总额未完全支付完毕之前，由于订单内容发生变化，甲、乙双方可协商变更未支付运费部分的现金部分的数额，具体变更情况由双方另行 签署补充协议确定。</view>
+		<view class="contract-page__mintitle">3.1.注册者资格</view>
+		<view class="contract-page__content">您确认，在您完成注册程序或以其他平台允许的方式实际使用平台服务时，您应当是具备完全民事权利能力和完全民事行为能力的自然人、法人或其他组织。若您不具备前述主体资格，则您及您的监护人应承担因此而导致的一切后果，且平台有权注销或永久冻结您的账号，并保留向您及您的监护人索偿的权利。</view>
 		
-		<view class="contract-page__title">三、乙方应按照运单约定，在约定的时限内，将货物送达到甲方指定的送达地。</view>
+		<view class="contract-page__mintitle">3.2.注册账号的渠道</view>
+		<view class="contract-page__content">中国电信基础产品用户默认自动开通平台，无需另行注册即可以使用平台各项互联网内容及应用服务以及平台合作的第三方互联网应用服务。您可以通过以下渠道进行账号注册，包括但不限于：</view>
+		<view class="contract-page__content">（1）手机短信注册</view>
+		<view class="contract-page__content">（2）手机号注册</view>
+		<view class="contract-page__content">（3）邮箱号注册</view>
+		<view class="contract-page__content">（4）第三方账号首次登录注册</view>
+		<view class="contract-page__content">（5）首次使用免密登录时注册</view>
+		<view class="contract-page__content">免密登录是天翼账号产品首创的一种快捷、安全的登录方式。天翼账号通过手机移动数据网络认证当前本机sim卡的手机号码，登录时免除输入免输帐号密码操作，一键登录并注册，同时确保手机号账号在本机上登录，不会被他人盗用。</view>
 		
-		<view class="contract-page__title">四、乙方按运单约定核对清点并接收待运输的货物后，视为运输义务的开始、保管责任的转移。乙方将货物送至运单指定地点，收货人未对货物数量提出异议，在“超好运”APP上卸货确认的，视为乙方已经按照运输单证的记载和要求交付货物，交付应视为运输义务的终止、保管责任的转移。</view>
+		<view class="contract-page__mintitle">3.3.您完成申请注册程序后，依照平台的业务规则获得天翼账号及189邮箱的使用权。</view>
+		<view class="contract-page__mintitle">3.4.无论您通过何种方式注册平台，均受本规则约束。</view>
+		<view class="contract-page__mintitle">3.5.用户信息</view>
+		<view class="contract-page__content">（1）在完成注册或激活流程时，若国家法律法规有特殊规定的，您需要填写真实的身份信息，请按相应页面的提示准确提供并及时更新您的资料，以使之真实、及时、完整和准确。如有合理理由怀疑您提供的资料错误、不实、过时或不完整的，平台有权向您发出询问及或要求改正的通知，并有权直接做出删除相应资料的处理，直至中止、终止对您提供部分或全部平台服务。</view>
+		<view class="contract-page__content">（2）您应当准确填写并及时更新您提供的电子邮件地址、联系电话、联系地址、邮政编码等联系方式，以便平台与您进行有效联系，因通过这些联系方式无法与您取得联系，导致您在使用平台服务过程中产生任何损失或增加费用的，应由您完全独自承担。您了解并同意，您有义务保持你提供的联系方式的有效性，如有变更或需要更新的，您应按平台的要求进行操作。</view>
+		<view class="contract-page__content">（3） 平台与用户一同致力于个人信息的保护，保护用户个人信息是平台的一项基本原则。未经您的同意，平台不会向平台以外的任何公司、组织和个人披露您的个人信息，但法律法规另有规定的除外。</view>
 		
-		<view class="contract-page__title">五、乙方应保证承运车辆技术状况良好，并能满足所运货物重量的要求。使用的车辆、容器应做到外观整洁，车体、容器内干净无污染物、残留物。</view>
+		<view class="contract-page__mintitle">3.6.账号安全</view>
+		<view class="contract-page__content">您须自行负责对您的平台账号和密码保密，您需要采取特定措施保护您的号码安全，包括但不限于妥善保管平台账号与密码、安装防病毒木马软件、定期更改密码等措施。且须对您在该账号和密码下发生的所有活动（包括但不限于信息披露、发布信息、网上点击同意或提交各类规则协议、网上续签协议或购买服务等）承担责任。根据平台及或合作第三方的要求，用户选择使用天翼平台账号登录使用互联网（或移动互联网）应用服务时，可能需要与平台及或合作第三方签订相关应用服务条款。无论是否签订相关条款，用户均应当自觉遵守平台及或合作第三方SPCP在应用服务中的特定要求及规定。</view>
+		<view class="contract-page__content">您同意：(a) 如发现任何人未经授权使用您的账号和密码，或发生违反保密规定的任何其他情况，您会立即通知平台； (b) 确保您在每个上网时段结束时，以正确步骤离开网站服务。平台不能也不会对因您未能遵守本款规定而发生的任何损失负责。您理解平台对您的请求采取行动需要合理时间，平台对在采取行动前已经产生的后果（包括但不限于您的任何损失）不承担任何责任。</view>
+		<view class="contract-page__mintitle">3.7.密码找回</view>
+		<view class="contract-page__content">如果您的平台账号被盗、密码遗忘或因其他原因导致无法正常登录，您可以按照平台的申诉途径进行申诉，平台并不承诺您一定能通过申诉找回账号密码。</view>
+		<view class="contract-page__mintitle">3.8.短信通知</view>
+		<view class="contract-page__content">您同意平台拥有向已经注册的用户发送订单信息、促销活动等商业性营销类短信通知的权利。</view>
+		<view class="contract-page__mintitle">3.9.邮件、电话通知</view>
+		<view class="contract-page__content">您同意平台拥有向已经注册的用户发送订单信息、促销活动等商业性营销类邮件、电话等形式通知的权利。</view>
 		
-		<view class="contract-page__title">六、如遇车辆发生意外导致货物的灭失、短少、损坏、变质、污染等问题，乙方必须第一时间通知甲方，并应尽一切努力减少货物损失。</view>
+		<view class="contract-page__title">四、用户行为规范</view>
+		<view class="contract-page__mintitle">4.1.您不得恶意注册平台账号。平台可以对恶意注册行为进行独立判断和处理。</view>
+		<view class="contract-page__mintitle">4.2.平台号码使用权仅属于初始申请注册人。未经平台许可，您不得赠与、借用、租用、转让或售卖平台号码或者以其他方式许可非初始申请注册人使用平台号码</view>
+		<view class="contract-page__mintitle">4.3.如果您当前使用的平台账号并不是您初始申请注册的或者通过平台提供的其他途径获得的，您不得用该号码登录或进行任何操作，并请您在第一时间通知平台或者该号码的初始申请注册人。</view>
 		
-		<view class="contract-page__title">七、乙方自行承担车辆运营需缴纳的各类费用（包括但不限于承担在运输过程中发生的费用包括燃油费用、驾驶人员工资福利、车辆通行费用、车辆维修费用、交通违章罚款、交通事故赔偿费用等），以及交强险和商业保险，自行承担运输期间发生的交通事故、车辆盗抢、货物差损等责任。在接受承运货物时，乙方有责任核对所承运的货物与订单信息是否相符，若不相符的，应及时向托运方和甲方反映；若发现有违禁物品的，应该拒绝承运；乙方在承接业务时应自行审查所承运的货物的资质等要求是否与乙方的条件相匹配，若发现无法匹配的，应该停止接单或终止承运。</view>
+		<view class="contract-page__title">五、服务协议的变更或终止</view>
+		<view class="contract-page__mintitle">5.1.出现以下情况时，平台有权进行独立判断并随时限制、冻结或终止您对平台账号的使用，并根据实际情况决定是否恢复使用：</view>
+		<view class="contract-page__content">（1）违反法律法规、平台各服务协议或业务规则的规定。</view>
+		<view class="contract-page__content">（2）平台发现您并非号码初始申请注册人，平台有权在未经通知的情况下终止您使用该号码</view>
+		<view class="contract-page__content">（3）为了充分利用号码资源，如果您存在注册平台账号后未及时进行初次登录使用，或长期未登录使用平台账号等情形，平台有权终止该账号的使用。</view>
+		<view class="contract-page__content">（4）本协议（含规则）变更时，您明示并通知平台不愿接受新的服务协议的。</view>
+		<view class="contract-page__content">（5）用户使用平台账号服务的权利可以是个人的，也可以是一个公司或实体的商业性组织。未经平台同意，用户不能利用平台账号相关服务进行销售或其他商业用途。</view>
+		<view class="contract-page__content">（6）本协议及本协议的补充协议规定的其他情形</view>
+		<view class="contract-page__mintitle">5.2.您同意，您与平台的合同关系终止后，平台仍享有下列权利</view>
+		<view class="contract-page__content">（1）继续保存您的用户信息及您使用平台服务期间的所有日志信息。</view>
+		<view class="contract-page__content">（2）您在使用平台服务期间存在违法行为或违反本协议和或规则的行为的，平台仍可依据本协议向您主张权利。</view>
+		<view class="contract-page__mintitle">5.3.中国电信手机号码被拆机或销号满30天，平台将终止您的手机号码对应的相关产品服务：包括但不限于天翼云盘、189邮箱等；并注销您的天翼云盘账号、189邮箱账号、清理账号、别名、邮件内容、网盘资料等内容。天翼云盘、189邮箱等后续服务将由各产品负责。非中国电信手机号码被所属运营商拆机或销号的，请您及时自行注销相应手机号码的天翼账号等，平台不承担由此引发的客户数据丢失等责任及纠纷。</view>
 		
-		<view class="contract-page__title">八、若乙方在运输过程中使用的移动设备由甲方提供，则乙方在运输工程中负责该设备的保管。若运输途中设备遗失，则乙方原价赔偿。</view>
+		<view class="contract-page__title">六、客户服务</view>
+		<view class="contract-page__mintitle">6.1.如果您对平台采取的措施有异议或在使用过程中有其他问题的，均可联系平台客户服务部门处理，我们会给予您必要的帮助。</view>
+		<view class="contract-page__title">七、免责声明</view>
+		<view class="contract-page__mintitle">7.1.客户明确同意使用天翼账号服务的风险由客户承担。平台明确表示不提供任何类型的担保，不论是明确的或隐含的。平台将尽力维护客户使用天翼账号服务的合法权益，但不担保天翼账号服务一定能满足客户的要求，也不担保服务的及时性、安全性、真实性、稳定性、正确性。</view>
+		<view class="contract-page__mintitle">7.2.客户理解并接受下载或通过相应产品服务取得的任何信息资料取决于客户自己，并自行承担系统受损、资料丢失以及其它任何风险。综合平台对客户在互联网上得到的任何商品购物服务、交易进程、招聘信息等，都不作担保。</view>
+		<view class="contract-page__mintitle">7.3.中国电信对直接、间接、偶然、特殊及继起的损害不负责任，这些损害来自：不正当使用天翼账号服务及其相关应用服务，在网上购买商品或类似服务，在网上进行交易，非法使用服务或客户传送的信息有所变动。</view>
+		<view class="contract-page__mintitle">7.4.天翼账号对本项服务下涉及的境内外基础电信运营商的电信通信网络的故障、覆盖范围限制、不可抗力、客户所在位置、客户关机或其他非天翼账号技术能力范围内的事因等造成的服务中断、资料丢失、出现乱码、错误接收、无法接收、迟延接收不承担责任。</view>
+		<view class="contract-page__mintitle">7.5.平台保留判定客户的行为是否符合本服务条款要求的权利，如果客户违背了本服务条款的规定，综合平台有权中止或者终止相应天翼账号服务</view>
+		<view class="contract-page__mintitle">7.6.转户规定：如果客户将手机号码、固定电话号码转让给他人，请客户提前自行删除天翼账号内资料（含邮件、通讯录、网盘资料等个人数据）。否则，受让该号码的客户将能够查看您的客户个人数据。平台不承担由此引发的客户数据丢失等责任及纠纷。</view>
 		
-		<view class="contract-page__title">九、乙方授权甲方及超好运平台从以下两方面获取该运单项下车辆位置数据：1. 通过超好运软件基于GPS和移动网络获取；2.通过第三方机构获取当前位置、轨迹跟踪等定位信息。</view>
+		<view class="contract-page__title">八、隐私条款</view>
+		<view class="contract-page__mintitle">8.1.尊重用户个人隐私是中国电信的一项基本政策。平台会通过e-sim卡/流量等中国电信自有技术手段获取用户的手机号码信息，以便您可以
+		使用中国电信账号服务进行注册登录；但平台会合理、合法、安全地对您的手机号码信息进行存储与使用。同时，用户未办理停机保号业务且停机超过3个月以上的手机号，或者主动注销的手
+		机号，将被运营商（联通、移动、电信）回收。平台将反馈信息给业务使用方，原有用户信息将被清空。 中国电信使用自有技术及自行收集的用户信息（即用户办理移动电话入网时通过签订
+		用户入网协议等方式授权中国电信收集、使用的信息、数据）为用户提供服务、向用户发送提示。在用户许可（包括但不限于点击本机号码一键登录、确认使用账号服务一键登录、签订相关产
+		品用户协议及隐私政策、授权中国电信向第三方产品提供用户信息以便用户进行一键登录）之前，平台不会将用户的任何个人信息提供给任何第三方产品（包括公司或个人），除非平台根据
+		法律规定或政府强制要求公开个人资料，或因善意确信这样的作法符合下列任一项:</view>
+		<view class="contract-page__content">（1）符合法律公告或适用于平台服务的合法程序；</view>
+		<view class="contract-page__content">（2）保护平台的知识产权或其他重要权利；</view>
+		<view class="contract-page__content">（3）在紧急的情况下，竭力维护用户个人和社会大众的隐私安全。</view>
 		
-		<view class="contract-page__title">十、乙方依照本协议约定完成运单后（以托运方在超好运APP上确认卸货为准），甲方应按本协议第一条约定的运费及支付方式委托第三方通过“超好运”平台支付运费（现金）至乙方“超好运”平台账号。</view>
-		
-		<view class="contract-page__title">
-			十一、每单货物的实际运费以卸货时收货人测算的运输单位（每/件/每吨/每千克或其它运单约定的运输单位）数量为计算运费的依据，具体计算规则以乙方所使用的“超好运”平台上的页面展示为准。若因收货人在收货时输入错误或计算错误导致支付到司机平台账号中的运费比实际应该支付给司机的运费少，则乙方司机可以先和发货方物流企业沟通申请补款。发货方物流企业向甲方承运公司提出申请，待甲方承运公司审核情况属实之后，委托第三方通过“超好运”平台将运费差额补充支付到乙方司机的“超好运”平台账号中。同时发货方应将与本次运费差额补充支付相关的线上服务费按照发货方与甲方之间的约定支付给甲方。在乙方将货物交付收货人之前，因托运人要求乙方中止运输、返还货物、变更到达地或将货物交给其他收货人，因此增加的费用由乙方与托运方自行协商确认，增加的费用由乙方直接与托运方结算收款。
-		</view>
-		
-		<view class="contract-page__title">十二、违约责任</view>
-		<view class="contract-page__content">
-			<view>1.运输过程中出现任何货物的灭失、短少、损坏、变质、污染等货物价值减少、灭失等情形，由乙方承担货物毁损、灭失的损害赔偿责任，甲方有权停止支付按照本协议第一条约定本应支付给乙方的运费，该等运费仍不足以补偿乙方需承担的前述损害赔偿责任的，乙方需继续补偿。但乙方证明货物的毁损、灭失是因不可抗力、货物本身的自然性质或者合理损耗等原因造成的，乙方不承担损害赔偿责任。</view>
-			<view>2.乙方没有按时将货物在指定地点交给收货人的，每逾期一天，按当次运单约定运费百分之二的标准向甲方支付违约金。</view>
-			<view>3.乙方如将货物错运到货地点或接货人，应无偿运至合同规定的到货地点或接货人。如果货物逾期到达，乙方应偿付逾期交货的违约金。</view>
-			<view>4.本条约定的乙方应付的损害赔偿及违约金等款项，甲乙双方同意委托“超好运” 平台在应付乙方的其他运费中代扣，具体扣款金额甲乙双方另行协商确定。</view>
-		</view>
-		
-		<view class="contract-page__title">十三、通知与送达</view>
-		<view class="contract-page__content">本协议履行过程中，乙方有任何疑问应通过“超好运”平台或超好运指定客服电话联系甲方解决，甲方应通过本协议约定的联系电话以短信方式通知或通过“超好运”平台通知乙方。甲方的通知自发出之日起第三日视为对乙方送达成功及乙方对内容的默认，因此请乙方提供本人有效的联系电话。乙方需要变更联系电话的，应第一时间通过“超好运”平台操作变更，否则由此产生的不利后果由乙方自行承担。</view>
-		
-		<view class="contract-page__title">十四、本合同未尽事宜，双方可另行协商补充约定。</view>
-		
-		<view class="contract-page__title">十五、本合同在履行过程中发生争议，由双方协商解决，协商不成的，甲乙双方同意由甲方住所地的人民法院管辖审理双方的纠纷或争议。</view>
-		
-		<view class="contract-page__title">十六、本电子协议内容为甲乙双方真实意愿表示，在乙方确认后自动生效，双方均知晓并同意以上协议内容。</view>
-	
-		<view class="contract-page__after">
-			<view class="contract-page__after__box">
-				<view class="text">甲方签字：<text class="underline">{{ form.branchName }}</text></view>
-				<view class="text">(签章)</view>
-				<view class="text">时间：</view>
-			</view>
-			<view class="contract-page__after__box">
-				<view class="text">乙方签字：<text class="underline">{{ form.driverName }}</text></view>
-				<view class="text">(签章)</view>
-				<view class="text">时间：</view>
-			</view>
-		</view>
-		
+		<view class="contract-page__title">九、知识产权</view>
+		<view class="contract-page__mintitle">9.1.中国电信是天翼账号平台的所有权及知识产权权利人。上述平台服务指的是中国电信、或其关联公司、或其授权主体等通过平台为用户提供的包括但不限于信息发布分享、平台应用程序等功能、软件、服务等。</view>
+		<view class="contract-page__mintitle">9.2.本平台内容包括但不限于域名、商号、商标、文字、图形及图像、视像及声音和从相关许可方获得的信息，以及其他材料（统称“内容”）。</view>
+		<view class="contract-page__mintitle">9.3.本平台所载内容均受法律保护。除法律另有规定或中国电信与用户另有约定外，用户不得以商业目的对本网站内容进行修改、复制、分发、传送、显示、公布、许可、转让、销售或基于以上内容创建衍生作品的使用。如果用户违反法律规定或损害本网站和中国电信的利益，本平台保留采取相关法律措施的权利。</view>
+		<view class="contract-page__title">十、法律适用</view>
+		<view class="contract-page__mintitle">10.1.本协议之效力、解释、变更、执行与争议解决均适用中华人民共和国大陆地区法律，因使用平台所产生的争议，协商不成的，任何一方可以向被告所在地有管辖权的人民法院提起诉讼。</view>
+		<view class="contract-page__mintitle">10.2.本规则所有条款的标题仅为阅读方便，本身并无实际涵义，不能作为本规则涵义解释的依据。</view>
+		<view class="contract-page__mintitle">10.3.本规则条款无论因何种原因部分无效或不可执行，其余条款仍有效，对双方具有约束力。</view>
 	</view>
 </template>
 
 <script>
 	import { mapState } from 'vuex'
-	import { getContractByCode } from '@/config/service/protocol.js';
 	import WhiteHeader from '@/components/Header/WhiteHeader.vue';
 	export default {
 		components: {
@@ -153,43 +116,13 @@
 		data() {
 			return {
 				form: {
-					contractNo: 'DADAOCHENG2106041543464202',
-					createTime: '2021-06-04 15:43:45',
-					branchName: '福建大道成物流科技有限公司',
-					branchOrganizationCodeNo: '913713000673687316',
-					branchPhone: '4008270535',
-					driverName: '测试陈司机',
-					licenseNumber: '闽A059ED',
-					driverLicense: '1101011990',
-					driverIdentificationNumber: '11010119900307619X',
-					driverPhone: '139591090551',
-					startAddress: '',
-					consignor: '',
-					consignorPhone: '',
-					endAddress: '',
-					consignee: '',
-					consigneePhone: '',
-					goodsTypeName: '',
-					goodsBigTypeName: '',
-					loadWeight: '',
-					stowageStatus: '',
-					goodsName: '',
-					deliveryFeeDeserved: '',
-					loadTime: '',
-					orderRemark: ''
+					
 				}
 			}
 		},
 		async onLoad(option){
-			await this.$onLaunched;
-			this.getData(option.code);
 		},
 		methods: {
-			getData(code){
-				getContractByCode(code, this.headerInfo).then(response => {
-					this.form = { ...response.data }
-				});
-			}
 		}
 	}
 </script>
@@ -245,6 +178,11 @@
 		}
 	}
 	&__title{
+		font-size: 30rpx;
+		font-weight: 700;
+		margin: 30rpx 0;
+	}
+	&__mintitle{
 		font-size: 28rpx;
 		font-weight: 700;
 		margin: 30rpx 0;
