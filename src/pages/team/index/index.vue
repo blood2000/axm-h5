8ee69b38b41991cece7f3c99477b3fb1bff44c2a
huangst <313949553@qq.com>
2021-06-07 15:35:52 +0800

    车队统计页

---
+++ src/pages/team/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		车队统计页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
