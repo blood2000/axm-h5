77c437e021036782b274b5a60515b3c70b885f92
huangst <313949553@qq.com>
2021-06-07 20:47:46 +0800

    公共样式

--- src/pages/shipment/index/index.vue
+++ src/pages/shipment/index/index.vue
@@ -1,6 +1,59 @@
 <template>
-	<view>
-		货主统计页
+	<view class="shipment-index">
+		<view class="c-app-container">
+			<view class="ly-flex-pack-around" style="margin-bottom: 40rpx;">
+				<view class="c-count-box">
+					<view class="count">234<text class="unit">个</text></view>
+					<view class="label">项目<text class="has-arrow"></text></view>
+				</view>
+				<view class="c-count-box">
+					<view class="count">234<text class="unit">个</text></view>
+					<view class="label">货源<text class="has-arrow"></text></view>
+				</view>
+			</view>
+			<view class="ly-flex-pack-around">
+				<view class="c-count-box">
+					<view class="count">234<text class="unit">单</text></view>
+					<view class="label">运费完成<text class="has-arrow"></text></view>
+				</view>
+				<view class="c-count-box">
+					<view class="count">234<text class="unit">元</text></view>
+					<view class="label">实付运费<text class="has-arrow"></text></view>
+				</view>
+				<view class="c-count-box">
+					<view class="count">234<text class="unit">元</text></view>
+					<view class="label">开票<text class="has-arrow"></text></view>
+				</view>
+			</view>
+		</view>
+		
+		<view class="c-app-container">
+			<view class="c-title-box ly-flex-pack-justify ly-flex-align-center">
+				<text class="text">货源统计</text>
+				<text class="button">查看更多</text>
+			</view>
+		</view>
+		
+		<view class="c-app-container">
+			<view class="c-title-box ly-flex-pack-justify ly-flex-align-center">
+				<text class="text">运输统计</text>
+				<text class="button">查看更多</text>
+			</view>
+		</view>
+		
+		<view class="c-app-container">
+			<view class="c-title-box ly-flex-pack-justify ly-flex-align-center">
+				<text class="text">运费统计</text>
+				<text class="button">查看更多</text>
+			</view>
+		</view>
+		
+		<view class="c-app-container">
+			<view class="c-title-box ly-flex-pack-justify ly-flex-align-center">
+				<text class="text">开票统计</text>
+				<text class="button">查看更多</text>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -18,5 +71,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+.shipment-index{
+	
+}
 </style>
