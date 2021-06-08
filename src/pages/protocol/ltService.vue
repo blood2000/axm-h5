7e8371975cd719b2f95032a615b715c93818a39e
huangst <313949553@qq.com>
2021-06-08 17:18:45 +0800

    中国联通服务条款和隐私政策

---
+++ src/pages/protocol/ltService.vue
@@ -0,0 +1,74 @@
+<template>
+	<view class="service-page">
+		<view class="service-page__header">中国联通服务条款和隐私政策</view>
+		
+		<view class="service-page__title">一、【服务概述】</view>
+		<view class="service-page__content">本服务条款和隐私政策是用户（您）与中国联合网络通信有限公司（下称“本公司”、“中国联通”）之间的协议，使用包括中国联通所运营的“手机号一键登录”服务。本公司在此特别提醒用户认真阅读、充分理解本《服务条款和隐私政策》（下称《协议》）—— 用户应认真阅读、充分理解本《协议》中各条款，包括免除或者限制本公司责任的免责条款。请您慎重阅读并选择接受或不接受本《协议》（未成年人应在法定监护人陪同下阅读）。除非您接受本《协议》所有条款，否则您无权使用“手机号一键登录”服务。您的登录、使用等行为将视为对本《协议》的接受、认可，并同意接受本《协议》各项条款的约束。</view>
+		<view class="service-page__content">
+			本《协议》是您（下称“用户”）与中国联通之间关于用户登录、使用“手机号一键登录”服务所订立的协议。本《协议》描述本公司与用户之间关于“手机号一键登录”服务相关方面的权利义务。“用户”是指登录、使用、浏览本服务的个人或组织。 您对本协议的接受即受全部条款的约束，包括接受中国联通对本服务条款随时所做的任何修改。您同意本公司有权随时对本《协议》及相应的服务规则内容进行单方面的变更，并以消息推送、网页公告等方式予以公布，无需另行单独通知您；若您在本《协议》内容公告变更后继续使用本服务的，表示您已充分阅读、理解并接受修改后的协议内容，也将遵循修改后的协议内容使用本服务；若您不同意修改后的协议内容，请立即停止使用“手机号一键登录”提供的服务。</view>
+		
+		<view class="service-page__title">二、【用户个人信息保护】</view>
+		<view class="service-page__content">2.1、用户信息范围：本协议所称的用户信息是指符合法律、法规及相关规定，并符合下述范围的信息（用户使用本服务时中国联通获取到的信息，包括网络类型、屏幕尺寸、IP地址、手机号码），中国联通不存储以上信息，仅在用户登录过程中使用。</view>
+		<view class="service-page__content">2.2、本公司采用行业标准惯例以保护用户的个人信息，除法律法规另有规定或有关法律要求向司法机关和政府部门提供外，未经您的同意，本公司不会向第三方以外的任何公司、组织和个人披露您的个人信息。</view>
+		<view class="service-page__content">2.3、本公司会有完善的管理制度和安全措施来保护您的个人信息，以免遭受未经授权的访问、使用或披露。但鉴于技术限制，本公司不能确保用户的全部私人通讯及其他个人资料不会通过非本公司故意泄露之外的途径泄露出去，对此本公司不承担任何责任。</view>
+		<view class="service-page__content">2.4、由于本SDK用于登录场景，本公司在您同意下，将您的手机号码同步给您同意使用的APP上。</view>
+		
+		<view class="service-page__title">三、【服务风险及免责声明】</view>
+		<view class="service-page__content">3.1、您须明白，本服务仅依其当前所呈现的状况提供，本服务涉及到互联网及相关通信等服务，可能会受到各个环节不稳定因素的影响。因此服务存在因上述不可抗力、计算机病毒或黑客攻击、系统不稳定、用户所在位置、用户关机、通信网络、互联网络原因等造成的服务中断或不能满足用户要求的风险。使用服务的用户须承担以上风险，本公司对服务之及时性、安全性、准确性不作担保。</view>
+		<view class="service-page__content">3.2、本公司的系统发生故障影响到本服务的正常运行，本公司承诺及时与相关单位配合，及时处理进行修复。此外，为保证本公司提供服务的流畅性，本公司将在中国联通网站做出相应公示后，对系统进行维修保养、升级或其他目的而暂停提供本服务。</view>
+		<view class="service-page__content">3.3、您自本公司工作人员或经由本服务取得的建议或资讯，无论其为书面或口头，均不构成本公司的任何保证，中国联通对此不承担任何责任。</view>
+		<view class="service-page__content">3.4、若您允许手机自动向本服务发送短信以达到一键登录功能，所产生一切费用及后果本公司不承担任何责任。</view>
+		
+		<view class="service-page__title">四、【法律适用】</view>
+		<view class="service-page__content">4.1、本服务条款要与国家相关法律、法规一致，如发生服务条款与相关法律、法规条款有相抵触的内容，抵触部分以法律、法规条款为准。</view>
+		
+		<view class="service-page__title">五、【争议管辖条款】</view>
+		<view class="service-page__content">本协议之效力、解释、变更、执行与争议解决均适用中华人民共和国法律。因本协议产生之争议，均应依照中华人民共和国法律予以处理。若用户和中国联通之间发生任何纠纷或争议，首先应友好协商解决，协商不成的，用户在此完全同意将纠纷或争议提交中国联通主营业务所在地有管辖权的人民法院（即北京市西城区人民法院）管辖。</view>
+		
+		<view class="service-page__title">六、【保障】</view>
+		<view class="service-page__content">6.1、用户同意保障和维护本公司全体成员的利益，负责支付由用户使用超出服务范围或不当使用服务引起的一切费用（包括但不限于：律师费用、违反服务条款的损害补偿费用以及其它第三方使用用户的电脑、帐号和其它知识产权的追索费）。</view>
+		<view class="service-page__content">6.2、用户须对违反国家法律规定及本服务条款所产生的一切后果承担法律责任。</view>
+		
+		<view class="service-page__title">七、【其它】</view>
+		<view class="service-page__content">7.1、如本服务条款中的任何条款无论因何种原因完全或部分无效或不具有执行力，本服务条款的其余条款仍应有效且具有约束力，并且努力使该规定反映之意向具备效力。</view>
+		<view class="service-page__content">7.2、本服务条款构成您与中国联通之服务协议，规范您对本服务之使用，在您使用相关服务、或使用第三方提供的内容或软件时，亦应遵从所适用之附加条款及义务。</view>
+		<view class="service-page__content">7.3、中国联通对于本协议条款在法律范围内进行解释说明。用户对服务之任何部分或本服务条款的任何部分之意见及建议可联系本公司，客服电话：4000096800。</view>
+		
+		<view class="service-page__content">中国联合网络通信有限公司</view>
+		
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+	.service-page{
+		height: 100%;
+		padding: 0 60rpx;
+		background: #fff;
+		color: #444;
+		overflow-y: auto;
+		// padding-top: 130upx;
+		&__header{
+			text-align: center;
+			font-size: 36rpx;
+			font-weight: bold;
+			line-height: 100rpx;
+			padding-top: 30rpx;
+			color: #333;
+		}
+		&__title{
+			font-size: 28rpx;
+			font-weight: 700;
+			margin: 30rpx 0;
+		}
+		&__content{
+			font-size: 28rpx;
+			// text-indent: 2em;
+			line-height: 40rpx;
+			word-break: break-all;
+			margin-bottom: 40rpx;
+		}
+	}
+</style>
