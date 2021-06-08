bd79fb1ec2e6e8badd29de8f5d27768a235a4314
huangst <313949553@qq.com>
2021-06-08 18:02:43 +0800

    test

--- src/pages/startPage/index.vue
+++ src/pages/startPage/index.vue
@@ -28,11 +28,11 @@
 			getData(){
 				getPageJump(this.headerInfo).then(response => {
 					const role = response.data;
-					uni.showToast({
-						title: '后端返回的数据后端返回的数据后端返回的数据后端返回的数后端返回的数据后端返回的数据后端返回的数据据后端返回的数据后端返回的数据后端返回的数据后端返回的数据后端返回的数据: '+JSON.stringify(response),
-						icon: 'none',
-						duration: 5000
-					});
+					// uni.showToast({
+					// 	title: '后端返回的数据: '+JSON.stringify(response),
+					// 	icon: 'none',
+					// 	duration: 5000
+					// });
 					// 1跳转司机统计 2跳转调度者统计 3跳转货主大宗统计 4跳转货主渣土统计
 					if (role === 1) {
 						uni.redirectTo({
