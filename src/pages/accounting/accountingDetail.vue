5581bd3f58f41133cd02c182f6cce911c5575b66
tangyr <tyr285687545@qq.com>
2021-12-22 18:59:48 +0800

    核算规则详情

--- src/pages/accounting/accountingDetail.vue
+++ src/pages/accounting/accountingDetail.vue
@@ -32,7 +32,7 @@
 						src="@/static/ic_weighing_arrow_down.png" />
 				</view>
 			</view>
-			<view style="width: 100%; height: 1upx; background-color: #EBEBEB;margin-top: 28upx;" />
+			<view style="width: 100%; height: 2upx; background-color: #EBEBEB;margin-top: 28upx;" />
 			<view class="cardRow" style="margin-top: 28upx;">
 				<view style="display: flex; justify-content: center;align-items: center;">
 					<text class="rowLabel">默认规则</text>
@@ -70,13 +70,13 @@
 					</view>
 				</view>
 				<view class="roleLossItem" style="margin-top: 28upx;">
-					<view style="display: flex; align-items: center; margin-top: ;">
+					<view class="shemeItem">
 						<view style="display: flex; flex-direction: row;">
 							<text class="rowLabel">路耗亏吨方案</text>
 							<image type="icon" class="required" style="width: 32upx; height: 32upx; "
 								src="@/static/ic_required.png" />
 						</view>
-						<view>
+						<view style="display: flex; flex-direction: column;">
 							<radio-group @change="onSchemeSelect" class="radioGroup">
 								<label class="uni-list-cell uni-list-cell-pd radioButton"
 									v-for="(item, index) in scheme" :key="item.schemeName">
@@ -90,9 +90,49 @@
 						</view>
 					</view>
 				</view>
+				<view class="shemeItem">
+					<view class="ruleFomelaView" style="margin-right: 24upx;">-</view>
+					<view class="shemeInput">
+						<input type="digit" :value="defaultSchemeMin" style="font-size: 26upx;" placeholder="最小值" />
+						<text style="color: #666666;">{{schemeUnit[currentsUnitSheme].schemeUnitName}}</text>
+					</view>
+					<view class="ruleFomelaView" style="margin-left: 24upx;margin-right: 24upx;">至</view>
+					<view class="shemeInput">
+						<input type="digit" :value="defaultSchemeMax" style="font-size: 26upx;" placeholder="最大值" />
+						<text style="color: #666666;">{{schemeUnit[currentsUnitSheme].schemeUnitName}}</text>
+					</view>
+				</view>
 			</view>
-
+			<view style="width: 100%; height: 1upx; background-color: #EBEBEB;margin-top: 28upx;" />
+			<view class="cardRow" style="margin-top: 28upx;">
+				<text class="rowLabel">扣费项目</text>
+				<image type="icon" class="required" style="width: 32upx; height: 32upx; "
+					src="@/static/ic_accounting_plus.png" @click="open" />
+			</view>
+			<view style="width: 100%; height: 1upx; background-color: #EBEBEB;margin-top: 28upx;" />
+			<view class="cardRow" style="margin-top: 28upx;">
+				<text class="rowLabel">补贴项目</text>
+				<image type="icon" class="required" style="width: 32upx; height: 32upx; "
+					src="@/static/ic_accounting_plus.png" @click="open" />
+			</view>
 		</view>
+
+		<uni-popup ref="popup" type="bottom">
+			<view class="projectDialog">
+				<radio-group style="width: 100%;">
+					<label style="width: 100%;"
+						v-for="(item, index) in projectList" :key="item.projectName">
+						<view class="projectItem">
+							<view>{{item.projectName}}</view>
+							<view>
+								<radio :value="item.projectName" :checked="index === currentProject" />
+							</view>
+						</view>
+						<view v-if="index < projectList.length - 1" style="width: 100%; height: 1upx; background-color: #EBEBEB;margin-top: 28upx;" />
+					</label>
+				</radio-group>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 
@@ -105,6 +145,7 @@
 		},
 		data() {
 			return {
+				isShowProject: false,
 				title: "新增核算规则",
 				accountingSelectName: "请选择计算公式",
 				accountingSelectCode: "",
@@ -134,12 +175,37 @@
 				}],
 				scheme: [{
 					schemeName: "定额",
-					schemeCode: "1",
+					schemeCode: "0",
 				}, {
 					schemeName: "定律",
-					schemeCode: "2",
+					schemeCode: "1",
 				}],
+				schemeUnit: [{
+					schemeUnitName: "kg/m³",
+				}, {
+					schemeUnitName: "%",
+				}],
+				defaultSchemeMin: "5.00",
+				defaultSchemeMax: "5.00",
+				currentsUnitSheme: 0,
 				currentsSheme: 0,
+				projectList: [{
+					projectName: "抹零规则",
+					projectCode: "0",
+				}, {
+					projectName: "油费",
+					projectCode: "1",
+				}, {
+					projectName: "ETC费用",
+					projectCode: "2",
+				}, {
+					projectName: "超时费",
+					projectCode: "3",
+				}, {
+					projectName: "装车费",
+					projectCode: "4",
+				}, ],
+				currentProject: 0,
 			}
 		},
 		methods: {
@@ -164,8 +230,28 @@
 			// 选择了亏吨方案
 			onSchemeSelect(e) {
 				const index = e.detail.value
-				console.log("选择亏吨方案:", index)
+				this.currentsUnitSheme = index
+				if (this.currentsUnitSheme == 0) {
+					this.defaultSchemeMin = "5.00"
+				} else {
+					this.defaultSchemeMin = "0.3"
+				}
 			},
+			//点击添加扣费项目
+			onDeductionPlusClick() {
+				console.log("点击添加扣费项目")
+				this.isShowProject = true
+			},
+			//点击添加补贴项目
+			onSubsidiesPlusClick() {
+				this.isShowProject = true
+				console.log("点击添加补贴项目：", this.isShowProject)
+			},
+			//展示项目选择popup
+			open() {
+				// 通过组件定义的ref调用uni-popup方法 ,如果传入参数 ，type 属性将失效 ，仅支持 ['top','left','bottom','right','center']
+				this.$refs.popup.open('top')
+			}
 		}
 	}
 </script>
@@ -252,6 +338,13 @@
 		text-align: right;
 	}
 
+	.ruleView {
+		display: flex;
+		flex-direction: r;
+		justify-content: flex-end;
+		align-items: center;
+	}
+
 	.ruleInput {
 		font-size: 26upx;
 		color: #333333;
@@ -260,11 +353,34 @@
 		background-color: #FFFFFF;
 		padding-right: 12upx;
 		justify-content: flex-end;
-		min-width: 400upx;
-		width: 400upx;
+		min-width: 356upx;
+		width: 356upx;
 		padding-left: 12upx;
 		min-height: 60upx;
 		text-align: right;
+	}
+
+	.shemeItem {
+		display: flex;
+		margin-top: 28upx;
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-end;
+	}
+
+	.shemeInput {
+		font-size: 24upx;
+		color: #333333;
+		background-color: #FFFFFF;
+		padding-right: 12upx;
+		min-width: 200upx;
+		align-items: center;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 200upx;
+		padding-left: 12upx;
+		min-height: 60upx;
 	}
 
 	.radioGroup {
@@ -282,5 +398,24 @@
 		justify-content: center;
 		font-size: 36upx;
 
+	}
+
+	.projectDialog {
+		background-color: #FFFFFF;
+		width: calc(80vw);
+		border-radius: 20upx;
+		text-align: center;
+	}
+
+	.projectItem {
+		display: flex;
+		width: 100%;
+		flex-direction: row;
+		font-size: 36upx;
+		transform: scale(0.7);
+		margin-top: 28upx;
+		margin-bottom: 28upx;
+		justify-content: space-between;
+		align-items: center;
 	}
 </style>
