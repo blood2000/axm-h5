96077a60154259dda553cf9cb9017ff2e299b57c
tangyr <tyr285687545@qq.com>
2021-12-24 15:08:46 +0800

    新增核算规则部分ui逻辑和接口对接

--- src/config/service/accounting.js
+++ src/config/service/accounting.js
@@ -2,12 +2,25 @@ import uniRequest from "../request.js";
 
 // 核算规则获取项目
 export function getAccountingProjectList(ruleType, config) {
-  return uniRequest({
-    url: '/transportation/ruleItem/list',
-    method: 'get',
-	data: {
-		ruleType: ruleType
-	},
-	headers: config
-  });
+	return uniRequest({
+		url: '/transportation/ruleItem/list',
+		method: 'get',
+		data: {
+			ruleType: ruleType
+		},
+		headers: config
+	});
+}
+
+// 获取字典
+export function getDict(dictType, config) {
+	return uniRequest({
+		url: '/system/dict/data/listByDict',
+		method: 'post',
+		params: {
+			dictType: dictType,
+			dictPid: 0,
+		},
+		headers: config
+	});
 }
